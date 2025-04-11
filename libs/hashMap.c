144e81baa7dba619174bd9ae693cf2a69a2c4058
methy-dino <gutotlima@gmail.com>
2025-04-11 11:02:20 -0300

    updated libraries.

--- libs/hashMap.c
+++ libs/hashMap.c
@@ -1,5 +1,6 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
 #include <limits.h>
 int mapErr = 0;
 #define MAP_NOSUCH 1
@@ -10,15 +11,15 @@ typedef struct {
 } Entry;
 typedef struct {
     Entry* entries;
-    unsigned int (*hashf)(void*);
-    // used for comparison between keys, if they are equal, it should return 0, if different, it should be != 0;
+    size_t (*hashf)(void*);
+    /* used for comparison between keys, if they are equal, it should return 0, if different, it should be != 0*/
     int (*compare)(void*, void*);
-    // used to free data storage.    
+    /* used to free data storage.*/    
     void (*free)(Entry*);
-    unsigned int length;
-    unsigned int occupied;
+    size_t length;
+    size_t occupied;
 } HashMap;
-HashMap* createMap(int length, unsigned int (*hash)(void*), int(*compare)(void*,void*),void (*freefn)(Entry*)){
+HashMap* createMap(size_t length, size_t (*hash)(void*), int(*compare)(void*,void*),void (*freefn)(Entry*)){
     HashMap* ret = malloc(sizeof(HashMap));
 	if (ret == NULL){
 		mapErr = MALLOC_FAIL;
@@ -30,8 +31,9 @@ HashMap* createMap(int length, unsigned int (*hash)(void*), int(*compare)(void*,
 			free(ret);
 			return NULL;
 		}
-    // sanitizing data, should be faster than calling calloc().
-    for (unsigned int i = 0; i < length; i++){
+    /* sanitizing data, should be faster than calling calloc().*/
+    size_t i = 0;
+		for (i = 0; i < length; i++){
         entries[i].key = NULL;
     }
     ret->entries = entries;
@@ -44,47 +46,48 @@ HashMap* createMap(int length, unsigned int (*hash)(void*), int(*compare)(void*,
 }
 /* it is a good idea to rehash a map after deleting an entry, since that will most likely alter the hashcode of the object*/
 int rehash(HashMap* map){
-	// cannot do in-place, since it will probably collide with things that may move after
-    unsigned int j = 0;
+	/* cannot do in-place, since it will probably collide with things that may move after */
+    size_t j = 0;
 	Entry* newE = malloc(sizeof(Entry)* map->length);
 	if (newE == NULL){
 		mapErr = MALLOC_FAIL;
 		return 1;
 	}
-	for (unsigned int i = 0; i < map->length; i++){
+	size_t i = 0;
+	for (i = 0; i < map->length; i++){
         	newE[i].key = NULL;
    	}
-	for (unsigned int i = 0; i < map->length; i++){
+	for (i = 0; i < map->length; i++){
 		if (map->entries[i].key != NULL){
-           		j = map->hashf(map->entries[i].key) % map->length;
-            		while (newE[j].key != NULL){
-                		if (j == map->length){
-                    			j=0;
-                		}
-                		j++;
-            		}
-            //printf("key at %d  moved to %d\n",i,j);
-            newE[j].key = map->entries[i].key;
-            newE[j].value = map->entries[i].value;
-       		 }
-    	}
+      j = map->hashf(map->entries[i].key) % map->length;
+      while (newE[j].key != NULL){
+     		if (j == map->length){
+					j=0;
+				}
+				j++;
+			}
+			newE[j].key = map->entries[i].key;
+			newE[j].value = map->entries[i].value;
+		}
+  }
 	free(map->entries);
 	map->entries = newE;
 	return 0;
 }
-int growMap(HashMap* map, unsigned int inc){
-    unsigned int newL = map->length+inc;
+int growMap(HashMap* map, size_t inc){
+    size_t newL = map->length+inc;
     Entry* newE = malloc(sizeof(Entry)* newL);
 		if (newE == NULL){
 			mapErr = MALLOC_FAIL;
 			return 1;
 		}
-    // sanitizing data, should be faster than calling calloc().
-    for (unsigned int i = 0; i < newL; i++){
+    /* sanitizing data, should be faster than calling calloc().*/
+    size_t i = 0;
+		for (i = 0; i < newL; i++){
         newE[i].key = NULL;
     }
-    unsigned int j = 0;
-    for (unsigned int i = 0; i < map->length; i++){
+    size_t j = 0;
+    for (i = 0; i < map->length; i++){
         if (map->entries[i].key != NULL){
             j = map->hashf(map->entries[i].key) % newL;
             while (newE[j].key != NULL){
@@ -93,8 +96,7 @@ int growMap(HashMap* map, unsigned int inc){
                 }
                 j++;
             }
-            //printf("key at %d  moved to %d\n",i,j);
-            newE[j].key = map->entries[i].key;
+						newE[j].key = map->entries[i].key;
             newE[j].value = map->entries[i].value;
         }
     }
@@ -104,7 +106,7 @@ int growMap(HashMap* map, unsigned int inc){
 		return 0;
 }
 int addPair(HashMap* map, void* key, void* val){
-    // if (map->occupied > 3 / 4 * map->length)
+    /*if (map->occupied > 3 / 4 * map->length)*/
     if (4 * map->occupied > 3 * map->length-1){
 			char rep = 0;
       while (growMap(map, map->length) && rep < 3){
@@ -116,50 +118,48 @@ int addPair(HashMap* map, void* key, void* val){
 				mapErr = 0;
 			}
     }
-    int index = map->hashf(key) % map->length;
+    size_t index = map->hashf(key) % map->length;
     while (map->entries[index].key != NULL){
       	if (map->compare(map->entries[index].key, key) == 0){
 					map->entries[index].key = key;
     			map->entries[index].value = val;
-					// this should give better order
+					/*this should give better order*/
 				}
 				index++;
         if (index == map->length){
             index = 0;
         }	
     }
-			//printf("added key at index %d\n", index);
+			/*printf("added key at index %d\n", index);*/
     map->entries[index].key = key;
     map->entries[index].value = val;
     map->occupied++;
 		return 0;
 }
-unsigned int hasKey(HashMap* map, void* key){
-    unsigned int index = map->hashf(key) % map->length;
-    unsigned int start = index;
+size_t hasKey(HashMap* map, void* key){
+    size_t index = map->hashf(key) % map->length;
+    size_t start = index;
     while (map->compare(map->entries[index].key, key) != 0){
         index++;
         if (index == map->length){
             index = 0;
         }
         if (index == start){
-            //printf("Invalid or nonexistant key");
-            return -1; //auto convert to uint max
+            return -1; /*converts to max*/
 				}
     }
     return index;
 }
-unsigned int removeKey(HashMap* map, void* key){
-    unsigned int index = map->hashf(key) % map->length;
-    unsigned int start = index;
+int removeKey(HashMap* map, void* key){
+    size_t index = map->hashf(key) % map->length;
+    size_t start = index;
     while (map->compare(map->entries[index].key, key) != 0){
         index++;
         if (index == map->length){
             index = 0;
         }
         if (index == start){
-            //printf("Invalid or nonexistant key");
-            return 1;
+					return 1;
         }
     }
     map->occupied--;
@@ -169,53 +169,61 @@ unsigned int removeKey(HashMap* map, void* key){
     return 0;
 }
 void* getValue(HashMap* map, void* key){
-    int index = map->hashf(key) % map->length;
-    int start = index;
-    while (map->compare(map->entries[index].key, key) != 0){
-        index++;
-        if (index == map->length){
-            index = 0;
-        }
-        if (index == start){
-						mapErr = MAP_NOSUCH;
-            return (void*)NULL;
-        }
-    }
-    return map->entries[index].value;
+  size_t index = map->hashf(key) % map->length;
+  size_t start = index;
+  while (map->compare(map->entries[index].key, key) != 0){
+		index++;
+		if (index == map->length){
+			index = 0;
+		}
+		if (index == start){
+			mapErr = MAP_NOSUCH;
+			return (void*)NULL;
+      }
+  }
+  return map->entries[index].value;
 }
 void clearMap(HashMap* map){
-    for (unsigned int i = 0; i < map->length; i++){
-        if (map->entries[i].key != NULL){
-            map->free(&map->entries[i]);
-        }
-    }
+  size_t i = 0;  
+	for (i = 0; i < map->length; i++){
+		if (map->entries[i].key != NULL){
+			map->free(&map->entries[i]);
+		}
+	}
 }
 void discardMap(HashMap* map){
-    for (unsigned int i = 0; i < map->length; i++){
-        if (map->entries[i].key != NULL){
-            map->free(&map->entries[i]);
-        }
+  size_t i = 0;  
+	for (i = 0; i < map->length; i++){
+		if (map->entries[i].key != NULL){
+			map->free(&map->entries[i]);
     }
-    free(map->entries);
-    free(map);
+	}
+	free(map->entries);
+	free(map);
 }
-// very shitty default hash function that should return on anything.
-unsigned int defHash(void* key){
-	return (int) ((char*)key)[0];
+/* very shitty default hash function that should return on anything.*/
+size_t defHash(void* key){
+	return ((char*)key)[0];
 }
 
-// function to hash null terminated strings.
-unsigned int strHash(void* key){
-	unsigned int value;
-	unsigned int charSize = sizeof(char);
+/* function to hash null terminated strings.*/
+size_t strHash(void* key){
+	size_t value;
+	size_t charSize = sizeof(char);
 	charSize = charSize * 8;
-    unsigned int i = 0;
+  size_t i = 0;
 	while (((char*)key)[i] != '\0'){
-		// the bit shift by charSize is to grant compatibility with other charset, such as UTF-16. 
+		/* the bit shift by charSize is to grant compatibility with other charset, such as UTF-16.*/ 
 		value = ((value << charSize) | ((char*)key)[i]) & 65033;
 		i++;
 	}
 	return value;
+}
+int strcmpWrap(void* strA, void* strB){
+	if (strB == NULL || strA == NULL){
+		return 1;
+	}
+	return strcmp(strA, strB);
 }
 void defaultFree(Entry* entry){
 	free(entry->key);
@@ -236,16 +244,20 @@ void debugPrintMap(HashMap* map, void (*printEntry)(Entry*), int verbosity){
 	if (verbosity > 0) {
 		printf("it has %u entries, of which %u are full\n", map->length, map->occupied);
 	}
-	printf("this is it's entry data:\n");
-	for (unsigned int i = 0; i < map->length; i++){
-        if (map->entries[i].key != NULL){
-		    printEntry(&map->entries[i]);
-        } else {
-            printf("NULL ENTRY\n");        
-        }
+	if (printEntry != NULL) {
+		printf("this is it's entry data:\n");
+		size_t i;
+		for (i = 0; i < map->length; i++){
+			printf("- - - - Entry %d - - - -\n", i);
+			if (map->entries[i].key != NULL){
+				printEntry(&map->entries[i]);
+			} else {
+        printf("NULL ENTRY\n");        
+      }
+		}
 	}
 }
-// there's not much I can do about printing, since there can be near infinite pairs, uh have this generic function.
+/* there's not much I can do about printing, since there can be near infinite pairs, uh have this generic function.*/
 void addressPrint(Entry* pair){
 	printf("key at %p and val at %p", pair->key, pair->value);
 }
